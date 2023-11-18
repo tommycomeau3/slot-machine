f3c500521d64cd3596d3e58b845eea5982c2a193
Thomas Comeau <122565955+tommycomeau3@users.noreply.github.com>
2023-11-17 17:26:53 -0800

    Print slots method

--- main.py
+++ main.py
@@ -19,8 +19,8 @@ def get_slot_machine_spin(rows, cols, symbols):
     for symbol, symbol_count in symbols.items():
         for _ in range(symbol_count):
             all_symbols.append(symbol)
-    
-    columns = [[],[],[]]
+
+    columns = []
     for _ in range(cols):
         column = []
         current_symbols = all_symbols[:]
@@ -28,10 +28,22 @@ def get_slot_machine_spin(rows, cols, symbols):
             value = random.choice(current_symbols)
             current_symbols.remove(value)
             column.append(value)
-        
+
         columns.append(column)
 
     return columns
+
+
+def print_slot_machine(columns):
+    for row in range(len(columns[0])):
+        for i, column in enumerate(columns):
+            if i != len(columns) - 1:
+                print(column[row], end=" | ")
+            else:
+                print(column[row], end="")
+
+        print()
+
 
 
 def deposit():
@@ -95,5 +107,9 @@ def main():
             break
 
     print(f"You are betting ${bet} on {lines}. Total bet is equal to: ${total_bet}")
+
+
+    slots = get_slot_machine_spin(ROWS, COLS, symbol_count)
+    print_slot_machine(slots)
 
 main()
